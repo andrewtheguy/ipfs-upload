5b27771b0b740206f3b68d81eb59176d386e45dc
Andrew Chen <andrew@jaanuu.com>
2021-10-24 21:55:57 -0700

    working

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import * as IPFS from 'ipfs-core'
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 
 // async function test() {
 //   const ipfs = await IPFS.create();
@@ -14,12 +14,46 @@ import {useEffect, useState} from "react";
 // test();
 
 
+class FileInput extends React.Component {
+
+
+  constructor(props) {
+    super(props);
+    this.fileInput = React.createRef();
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    //alert(
+    //    `Selected file - ${this.fileInput.current.files[0].name}`);
+    this.props.onFileChange(this.fileInput.current.files[0]);
+  }
+
+  render() {
+    return (
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label>Upload file:</label>
+            <input type="file" className="form-control-file" ref={this.fileInput}/>
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+
+        </form>
+    );
+  }
+}
+
 function App() {
 
   const [ipfs,setIpfs] = useState(null);
 
   const [url,setUrl] = useState(null);
-  const [data,setData] = useState('');
+  const [file,setFile] = useState(null);
+  const [data,setData] = useState(null);
+
+  const changeFile = (file) => {
+    setFile(file);
+  }
 
   useEffect(()=>{
     (async () => {
@@ -36,18 +70,38 @@ function App() {
     }
   },[])
 
-  async function update() {
-    const {cid} = await ipfs.add(data);
-    const cidStr = cid.toString();
-    setUrl(`https://ipfs.io/ipfs/${cidStr}`)
-  }
+  useEffect(()=>{
+    if(!file){
+      setData(null);
+    }else{
+      const reader = new FileReader()
+      reader.onloadend = () => {
+        const buffer = Buffer.from(reader.result)
+        setData(buffer)
+      }
+      reader.readAsArrayBuffer(file)
+    }
+  },[file])
+
+  useEffect(()=>{
+    if(!data){
+      //console.log('nothing to update');
+      return;
+    }
+    console.log(data);
+    (async () => {
+      const {cid} = await ipfs.add(data);
+      const cidStr = cid.toString();
+      setUrl(`https://ipfs.io/ipfs/${cidStr}`)
+    })();
+  },[data])
 
   return (
     <div className="App">
       <section>
-        <p>
-        <textarea value={data} onChange={(e)=>setData(e.target.value)}/><button onClick={(e)=>update()}>update</button>
-        </p>
+        <div>
+          <FileInput onFileChange={changeFile}/>
+        </div>
         <p>url: {url && <span>{url}</span>}</p>
       </section>
     </div>
